da394e3e4c673d385c708e470279e24966332f61
Marcelo Borges~ <mfavaro@actioncargo.com.br>
2024-01-07 16:19:01 -0300

    feat: tirando filtro dos medicos

--- src/components/PageStrutures/AuthHome/MedicHomePage.tsx
+++ src/components/PageStrutures/AuthHome/MedicHomePage.tsx
@@ -68,25 +68,6 @@ export const MedicHomePage = ({ session }: Props) => {
         </div>
       ) : (
         <div className="w-full h-full">
-          <div className="rounded-lg border bg-card text-card-foreground shadow-sm flex flex-col md:flex-row flex-wrap items-center justify-around md:justify-start gap-2 w-full p-2 my-2">
-            <div>
-              <p>Filtros:</p>
-            </div>
-            <div className="py-1 sm:py-0">
-              <SingleDate
-                value={filters.initial}
-                label="Data Inicial:"
-                setValue={handleInitial}
-              />
-            </div>
-            <div className="py-1 sm:py-0">
-              <SingleDate
-                value={filters.finish}
-                label="Data Final:"
-                setValue={handleFinal}
-              />
-            </div>
-          </div>
           <div className="flex flex-wrap gap-2">
             {me?.openings && me?.openings?.length > 0 ? (
               me?.openings?.map((x) => (
